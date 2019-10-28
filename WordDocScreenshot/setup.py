74c81d28a8687609d82f4630cf66c4fc432be9b9
soumyaevan <sensoumya94@gmail.com>
2019-10-28 22:37:55 +0100

    updated worddocscreenshot

---
+++ WordDocScreenshot/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='WordDocScreenShot',
+    version='0.0.1',
+    description='Create Document from screenshot',
+    py_modules=["WordDocScreenShot"],
+    package_dir={'':'src'},
+)
