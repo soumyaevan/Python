693463edf0357999c4e9d9be75cc81fbdeb53c73
soumyaevan <sensoumya94@gmail.com>
2019-10-29 21:45:44 +0100

    packaging

--- WordDocScreenshot/rbfWordDocScreenShot/__init__.py
+++ WordDocScreenshot/rbfWordDocScreenShot/__init__.py
@@ -16,7 +16,6 @@ import warnings
 
 from robot.api import logger
 from robot.libraries.BuiltIn import BuiltIn
-
 from .rbfWordDocScreenShot import rbfWordDocScreenShot
 from .version import __version__
 
--- WordDocScreenshot/setup.py
+++ WordDocScreenshot/setup.py
@@ -1,9 +1,32 @@
-from setuptools import setup
+import re
+from os.path import abspath, dirname, join
+from setuptools import setup, find_packages
+
+
+CURDIR = dirname(abspath(__file__))
+
+CLASSIFIERS = '''
+Development Status :: 5 - Production/Stable
+License :: OSI Approved :: Apache Software License
+Operating System :: OS Independent
+Programming Language :: Python :: 3
+Topic :: Software Development :: Testing
+Framework :: Robot Framework :: Library
+'''.strip().splitlines()
+with open(join(CURDIR, 'version.py'), encoding="utf-8") as f:
+    VERSION = re.search("__version__ = '(.*)'", f.read()).group(1)
 
 setup(
-    name='WordDocScreenShot',
-    version='0.0.1',
-    description='Create Document from screenshot',
-    py_modules=["WordDocScreenShot"],
-    package_dir={'':'src'},
+    name='rbfWordDocScreenShot',
+    version=VERSION,
+    description="Robot Framework test library for storing screenshot in Word document",
+    author="Soumya Sen",
+    author_email='sensoumya94@gmail.com',
+    url='https://github.com/soumyaevan/rbfWordDocScreenshot.git',
+    license          = 'Apache License 2.0',
+    keywords         = 'robotframework testing testautomation',
+    platforms        = 'any',
+    install_requires = ['python-docx','robotframework','shutil','os'],  
+    classifiers=CLASSIFIERS,
+    packages=['rbfWordDocScreenShot']
 )
