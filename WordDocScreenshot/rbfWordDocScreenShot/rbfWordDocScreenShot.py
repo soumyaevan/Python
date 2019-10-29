f2dfbd4614ebbba49a884914c1821492348c0dff
soumyaevan <sensoumya94@gmail.com>
2019-10-29 20:51:58 +0100

    packaging

--- WordDocScreenshot/rbfWordDocScreenShot/rbfWordDocScreenShot.py
+++ WordDocScreenshot/rbfWordDocScreenShot/rbfWordDocScreenShot.py
@@ -1,9 +1,13 @@
 from docx import Document
 from docx.shared import Inches
+from robot.api import logger
+from robot.api.deco import keyword
 import shutil
 import os
-class WordDocScreenShot():
+
+class rbfWordDocScreenShot():
     
+    @keyword
     def createDoc(self,title,scPath):
         document = Document()
         #self.document.add_heading(title,0)        
@@ -11,11 +15,13 @@ class WordDocScreenShot():
             if fileName.endswith('.png'):
                 pg = document.add_paragraph()
                 r = pg.add_run()
-                print(scPath+"\\"+fileName)
+                logger.info("Word file created in "+scPath+"\\"+fileName)
                 r.add_picture(scPath+"\\"+fileName,width=Inches(5.8))
+                logger.info("Image "+ fileName + " has been added in word file")
                 #os.remove(scPath+"\\"+fileName)
         document.save(title+'.docx')
 
+    @keyword
     def cleanScDir(self,scPath):
         for fileName in os.listdir(scPath):
             if fileName.endswith('.png'):
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='WordDocScreenShot',
-    version='0.0.1',
-    description='Create Document from screenshot',
-    py_modules=["WordDocScreenShot"],
-    package_dir={'':'src'},
-)
